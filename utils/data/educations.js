736dbd4c882d5e481e5cfd8a7efac2126541d9ea
DinithKumudika <dinithwalpitagama@gmail.com>
2025-01-11 02:40:40 +0530

    clean up

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -5,22 +5,22 @@ export const educations = [
     duration: "2021 - Present",
     result: "Second Upper Division (3.6684 CPGA)",
     institution: "University of Colombo School of Computing",
-    institutionLogo: "/public/image/ucsc.png"
+    institutionLogo: "/image/ucsc.png"
   },
   {
     id: 2,
     title: "GCE A/L Examination",
-    duration: "2018",
-    result: "A - 9",
+    duration: "2019",
+    result: "B - Combined Maths, C - Chemistry, C - Physics",
     institution: "Rahula College, Matara",
-    institutionLogo: "/public/image/rahula.png"
+    institutionLogo: "/image/rahula.png"
   },
   {
     id: 3,
     title: "GCE O/L Examination",
     duration: "2015",
-    result: "B - Combined Maths, C - Chemistry, C - Physics",
+    result: "A - 9",
     institution: "Rahula College, Matara",
-    institutionLogo: "/public/image/rahula.png"
+    institutionLogo: "/image/rahula.png"
   }
 ]
