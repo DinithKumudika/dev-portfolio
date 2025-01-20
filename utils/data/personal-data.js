69418cb8d9823c44d6a20b965c867f1a1bdc7339
DinithKumudika <dinithwalpitagama@gmail.com>
2025-01-20 16:03:56 +0530

    about section update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Dinith Kumudika",
   profile: '/profile.jpeg',
   designation: "Software Engineer",
-  description: "My name is Dinith Kumudika. I am a professional and enthusiastic developer and a quick learner with a self-learning attitude. Currently I am Undergraduate student at University of Colombo School of Computing (UCSC), pursuing a Bachelor of Information Systems Honours degree. I love to learn and explore new technologies and am passionate about problem-solving. My main interests lies with Backend Web Development, Mobile Application Development and Generative AI. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Dinith Kumudika. I am a professional and enthusiastic developer and a quick learner with a self-learning attitude. Currently I am Undergraduate student at University of Colombo School of Computing (UCSC), pursuing a Bachelor of Information Systems Honours degree. I love to learn and explore new technologies and am passionate about problem-solving. My main interests lies with Backend Web Development, Mobile Application Development and Generative AI. I am available for software engineering job opportunities that suits my skills and interests.",
   email: 'dinithkumudika@gmail.com',
   phone: '+94712223456',
   address: 'Kohuwala, Sri Lanka',
